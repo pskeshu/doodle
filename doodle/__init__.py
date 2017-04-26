f94a815242f28ebaa3137bb4ae6dcff648a8f970
Kesavan PS <pskesavan@tifrh.res.in>
2017-04-26 13:13:27 +0530

    structured the module

---
+++ doodle/__init__.py
@@ -0,0 +1,5 @@
+from .manual import manual
+
+__version__ = 0.1
+
+__all__ = ['manual']
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='doodle',
+      version='0.1',
+      description='A library to segment objects in an image in a semi-automated fashion.',
+      url='https://github.com/pskeshu/doodle',
+      author='Kesavan Subburam',
+      author_email='pskesavan@tifrh.res.in',
+      license='MIT',
+      packages=['doodle'],
+      zip_safe=False)
